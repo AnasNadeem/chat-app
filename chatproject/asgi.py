fdb96e2693c11a4d05b3f6b71db73b29f73d92eb
Anas Nadeem <anas5678go@gmail.com>
2022-01-25 13:26:00 +0530

    Routing and Consumer added

--- chatproject/asgi.py
+++ chatproject/asgi.py
@@ -1,21 +1,17 @@
-"""
-ASGI config for chatproject project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
-"""
-
 import os
-
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+import chatapp.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatproject.settings')
 
 # application = get_asgi_application()
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    # Just HTTP for now. (We can add other protocols later.)
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chatapp.routing.websocket_urlpatterns
+        )
+    )
 })
